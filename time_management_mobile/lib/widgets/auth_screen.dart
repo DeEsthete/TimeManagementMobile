692421071e1dfae478ac5ff0d63b5c0195eb8cf9
JeLion <djoniqaz@gmail.com>
2020-07-26 18:07:40 +0600

    feat(widgets) добавил BadResponseLayout

--- time_management_mobile/lib/widgets/auth_screen.dart
+++ time_management_mobile/lib/widgets/auth_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:time_management_mobile/models/session_model.dart';
 import 'package:time_management_mobile/utils/translator.dart';
+import 'package:time_management_mobile/widgets/base/bad_response_layout.dart';
 
 class AuthScreen extends StatelessWidget {
   final TextEditingController _userNameController = new TextEditingController(
@@ -16,50 +17,52 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var sessionModel = Provider.of<SessionModel>(context);
     return Scaffold(
-      body: Container(
-        child: Form(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              TextFormField(
-                controller: _userNameController,
-                decoration: InputDecoration(
-                  hintText: Translator.of(context).translate("Username"),
-                ),
-              ),
-              TextFormField(
-                controller: _passwordController,
-                decoration: InputDecoration(
-                  hintText: Translator.of(context).translate("Password"),
-                ),
-                obscureText: true,
-              ),
-              RaisedButton(
-                onPressed: () => {
-                  sessionModel.logIn(
-                    _userNameController.value.text,
-                    _passwordController.value.text,
-                  )
-                },
-                child: Center(
-                  child: Text(
-                    Translator.of(context).translate("Sign in"),
+      body: BadResponseLayout(
+        child: Container(
+          child: Form(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                TextFormField(
+                  controller: _userNameController,
+                  decoration: InputDecoration(
+                    hintText: Translator.of(context).translate("Username"),
                   ),
                 ),
-              ),
-              FlatButton(
-                // TODO: Добавить navigation на регистрацию
-                onPressed: () => {},
-                child: Center(
+                TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    hintText: Translator.of(context).translate("Password"),
+                  ),
+                  obscureText: true,
+                ),
+                RaisedButton(
+                  onPressed: () => {
+                    sessionModel.logIn(
+                      _userNameController.value.text,
+                      _passwordController.value.text,
+                    )
+                  },
                   child: Center(
                     child: Text(
-                      Translator.of(context).translate("Sign up"),
+                      Translator.of(context).translate("Sign in"),
                     ),
                   ),
                 ),
-              ),
-            ],
+                FlatButton(
+                  // TODO: Добавить navigation на регистрацию
+                  onPressed: () => {},
+                  child: Center(
+                    child: Center(
+                      child: Text(
+                        Translator.of(context).translate("Sign up"),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
