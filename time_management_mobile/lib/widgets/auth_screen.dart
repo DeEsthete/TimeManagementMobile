50cf0f13b4836e2c2c055294adac6a9379f3a3f8
JeLion <djoniqaz@gmail.com>
2020-07-28 00:33:49 +0600

    chore(auth_screen) редизайн

--- time_management_mobile/lib/widgets/auth_screen.dart
+++ time_management_mobile/lib/widgets/auth_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:time_management_mobile/constant/color_consts.dart';
 import 'package:time_management_mobile/models/session_model.dart';
 import 'package:time_management_mobile/utils/translator.dart';
 import 'package:time_management_mobile/widgets/base/bad_response_layout.dart';
@@ -17,55 +18,74 @@ class AuthScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     var sessionModel = Provider.of<SessionModel>(context);
     return Scaffold(
+      appBar: AppBar(
+        title: Center(
+          child: Text(
+            Translator.of(context).translate("Sign in "),
+          ),
+        ),
+      ),
       body: BadResponseLayout(
-        body: Container(
-          child: Form(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: <Widget>[
-                Flexible(
-                  child: TextFormField(
-                    controller: _userNameController,
-                    decoration: InputDecoration(
-                      hintText: Translator.of(context).translate("Username"),
-                    ),
-                  ),
-                ),
-                Flexible(
-                  child: TextFormField(
-                    controller: _passwordController,
-                    decoration: InputDecoration(
-                      hintText: Translator.of(context).translate("Password"),
-                    ),
-                    obscureText: true,
-                  ),
-                ),
-                RaisedButton(
-                  onPressed: () => {
-                    sessionModel.logIn(
-                      _userNameController.value.text,
-                      _passwordController.value.text,
-                    )
-                  },
-                  child: Center(
-                    child: Text(
-                      Translator.of(context).translate("Sign in"),
-                    ),
-                  ),
-                ),
-                FlatButton(
-                  // TODO: Добавить navigation на регистрацию
-                  onPressed: () => {},
-                  child: Center(
-                    child: Center(
-                      child: Text(
-                        Translator.of(context).translate("Sign up"),
+        body: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Container(
+            child: Form(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Flexible(
+                    child: TextFormField(
+                      controller: _userNameController,
+                      decoration: InputDecoration(
+                        hintText: Translator.of(context).translate("Username"),
                       ),
                     ),
                   ),
-                ),
-              ],
+                  Flexible(
+                    child: TextFormField(
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        hintText: Translator.of(context).translate("Password"),
+                      ),
+                      obscureText: true,
+                    ),
+                  ),
+                  Container(
+                    margin: EdgeInsets.only(top: 20),
+                    width: 200,
+                    child: RaisedButton(
+                      onPressed: () => {
+                        sessionModel.logIn(
+                          _userNameController.value.text,
+                          _passwordController.value.text,
+                        )
+                      },
+                      child: Center(
+                        child: Text(
+                          Translator.of(context).translate("Sign in"),
+                          style: TextStyle(color: AppColors.alterFontColor),
+                        ),
+                      ),
+                      color: AppColors.primaryColor,
+                    ),
+                  ),
+                  Container(
+                    width: 200,
+                    child: FlatButton(
+                      // TODO: Добавить navigation на регистрацию
+                      onPressed: () => {},
+                      child: Center(
+                        child: Center(
+                          child: Text(
+                            Translator.of(context).translate("Sign up"),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
