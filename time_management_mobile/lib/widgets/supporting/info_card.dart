af3239a53e94bbe57d6dea9daa35de5498dc7818
JeLion <djoniqaz@gmail.com>
2020-08-04 18:49:56 +0600

    chore(info_card) добавил title

--- time_management_mobile/lib/widgets/supporting/info_card.dart
+++ time_management_mobile/lib/widgets/supporting/info_card.dart
@@ -1,15 +1,20 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:time_management_mobile/constant/color_consts.dart';
+import 'package:time_management_mobile/utils/translator.dart';
+import 'package:tinycolor/tinycolor.dart';
 
 class InfoCard extends StatelessWidget {
   static const double defaultPadding = 12.0;
+  static const double defaultRadius = 6.0;
 
   final Widget child;
+  final String title;
   final bool isRounded;
   final double paddingTop, paddingBottom, paddingLeft, paddingRight;
   InfoCard({
     @required this.child,
+    this.title,
     this.isRounded = true,
     this.paddingTop = defaultPadding,
     this.paddingBottom = defaultPadding,
@@ -37,9 +42,34 @@ class InfoCard extends StatelessWidget {
               offset: Offset(0, 3),
             ),
           ],
-          borderRadius: BorderRadius.circular(isRounded ? 6.0 : 0),
+          borderRadius: BorderRadius.circular(isRounded ? defaultRadius : 0),
         ),
-        child: child,
+        child: Column(
+          children: <Widget>[
+            title != null
+                ? Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                      color: TinyColor(AppColors.primaryColor).lighten(7).color,
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(InfoCard.defaultRadius),
+                        topRight: Radius.circular(InfoCard.defaultRadius),
+                      ),
+                    ),
+                    child: Center(
+                      child: Text(
+                        Translator.of(context).translate(title),
+                        style: TextStyle(
+                          color: AppColors.alterFontColor,
+                          fontSize: 22,
+                        ),
+                      ),
+                    ),
+                  )
+                : Container(),
+            child,
+          ],
+        ),
       ),
     );
   }
