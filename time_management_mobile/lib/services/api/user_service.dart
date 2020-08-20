be49a860acbc7ac7322484d1068f7d4732399a6d
JeLion <djoniqaz@gmail.com>
2020-08-21 00:48:20 +0600

    feat(auth_screen) добавил sign up

--- time_management_mobile/lib/services/api/user_service.dart
+++ time_management_mobile/lib/services/api/user_service.dart
@@ -5,6 +5,7 @@ import 'package:injectable/injectable.dart';
 import 'package:time_management_mobile/constant/filter_consts.dart';
 import 'package:time_management_mobile/constant/route_consts.dart';
 import 'package:time_management_mobile/dtos/authentication_dto.dart';
+import 'package:time_management_mobile/dtos/registration_dto.dart';
 import 'package:time_management_mobile/dtos/token_dto.dart';
 import 'package:time_management_mobile/services/app/http_service.dart';
 
@@ -21,6 +22,20 @@ class UserService {
 
     if (HttpResults.allowedHttpStatuses.contains(result.statusCode)) {
       return TokenDto.fromJson(result.body);
+    }
+
+    HttpService.badResponseSubject.add(result);
+    return Future.error(result);
+  }
+
+  Future registration(RegistrationDto registrationDto) async {
+    var result = await _client.post(
+      _root + 'registration',
+      body: registrationDto.toJson(),
+    );
+
+    if (HttpResults.allowedHttpStatuses.contains(result.statusCode)) {
+      return;
     }
 
     HttpService.badResponseSubject.add(result);
--- time_management_mobile/lib/widgets/screens/auth_screen.dart
+++ time_management_mobile/lib/widgets/screens/auth_screen.dart
@@ -1,17 +1,17 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:time_management_mobile/common/di_config.dart';
+import 'package:time_management_mobile/dtos/registration_dto.dart';
 import 'package:time_management_mobile/models/session_model.dart';
+import 'package:time_management_mobile/services/api/user_service.dart';
 import 'package:time_management_mobile/utils/translator.dart';
 import 'package:time_management_mobile/widgets/base/bad_response_layout.dart';
 
 class AuthScreen extends StatelessWidget {
-  final TextEditingController _userNameController = new TextEditingController(
-    text: "DefaultUser",
-  );
-  final TextEditingController _passwordController = new TextEditingController(
-    text: "DefaultUser",
-  );
+  final UserService _userService = getIt<UserService>();
+  final TextEditingController _userNameController = new TextEditingController();
+  final TextEditingController _passwordController = new TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -70,8 +70,14 @@ class AuthScreen extends StatelessWidget {
                   Container(
                     width: 200,
                     child: FlatButton(
-                      // TODO: Добавить регистрацию
-                      onPressed: () => {},
+                      onPressed: () => {
+                        showDialog(
+                          context: context,
+                          builder: (context) {
+                            return _buildRegistration(context);
+                          },
+                        )
+                      },
                       child: Center(
                         child: Center(
                           child: Text(
@@ -84,6 +90,100 @@ class AuthScreen extends StatelessWidget {
                 ],
               ),
             ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildRegistration(BuildContext context) {
+    final TextEditingController _userNameController =
+        new TextEditingController();
+    final TextEditingController _nickNameController =
+        new TextEditingController();
+    final TextEditingController _passwordController =
+        new TextEditingController();
+
+    return Dialog(
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Center(
+                child: Text(
+                  Translator.of(context).translate("Sign up"),
+                  style: TextStyle(
+                    fontSize: 22,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+              Flexible(
+                child: TextFormField(
+                  controller: _userNameController,
+                  decoration: InputDecoration(
+                    hintText: Translator.of(context).translate("Username"),
+                  ),
+                ),
+              ),
+              Flexible(
+                child: TextFormField(
+                  controller: _nickNameController,
+                  decoration: InputDecoration(
+                    hintText: Translator.of(context).translate("Password"),
+                  ),
+                ),
+              ),
+              Flexible(
+                child: TextFormField(
+                  controller: _passwordController,
+                  decoration: InputDecoration(
+                    hintText: Translator.of(context).translate("Password"),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(top: 20),
+                width: 200,
+                child: RaisedButton(
+                  onPressed: () => {
+                    _userService
+                        .registration(
+                          new RegistrationDto(
+                            nickname: _nickNameController.text,
+                            userName: _userNameController.text,
+                            password: _passwordController.text,
+                          ),
+                        )
+                        .then((value) => Navigator.pop(context)),
+                  },
+                  child: Center(
+                    child: Text(
+                      Translator.of(context).translate("Sign up"),
+                    ),
+                  ),
+                ),
+              ),
+              Container(
+                width: 200,
+                child: FlatButton(
+                  onPressed: () => {
+                    Navigator.pop(context),
+                  },
+                  child: Center(
+                    child: Center(
+                      child: Text(
+                        Translator.of(context).translate("Sign in"),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
           ),
         ),
       ),
